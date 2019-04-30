203b3e78699256da711642800e1c333356323974
Jake Logemann <820715+jakelogemann@users.noreply.github.com>
2019-04-29 22:07:10 -0400

    fix loader
    Signed-off-by: Jake Logemann <jake@lgmn.io>

--- nixos.nix
+++ nixos.nix
@@ -1,7 +1,11 @@
 # Used to install this framework's pkgs, lib & custom NixOS modules.
 # This is only useful in NixOS, Nix package users should instead use
 # `default.nix` which is ONLY the overlay.
-{ config, pkgs, lib, options, ... }: with lib; {
-  nixpkgs.overlays = mkAfter [(import ./default.nix)];
-  nix.nixPath      = mkAfter ["fnctlpkgs=${./allPackages.nix}"];
-}
+{ config, pkgs, lib, options, ... }: with lib;
+
+{ config = {
+
+  nixpkgs.overlays = [(import ./default.nix)];
+  nix.nixPath      = ["fnctlPkgs=${./.}"];
+
+}; }
